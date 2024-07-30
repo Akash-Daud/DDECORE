7a8e6010b911cc3cf624c883627ff3c0363569ba
Akash_Daud <daudakash2001@gmail.com>
2024-07-30 15:13:30 +0530

    first commit Akash

--- src/contexts/ModelControl.jsx
+++ src/contexts/ModelControl.jsx
@@ -30,18 +30,7 @@ export const CharacterAnimationsProvider = (props) => {
         "Sofa 3": "sofa3",
         "Sofa 4": "sofa4",
         "Sofa 5": "sofa5",
-        "Sofa 6": "sofa6",
-        "Chair 1": "chair1",
-        "Chair 2": "chair2",
-        "Table 1": "table1",
-        "Table 2": "table2",
-        "Table 3": "table3",
-        "Table 4": "table4",
-        "Table 5": "table5",
-        "Lamp 1": "lamp1",
-        "Lamp 2": "lamp2",
-        "Kitchen 1": "kitchen1",
-        "Elevator 1": "elevator1",
+       
       },
       onChange: (value) => {
         setCurrentModelName(value);
